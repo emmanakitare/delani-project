34f25fec327e4a55691af8260b8423b1a34b2f3a
emmanakitare <nakitare.wekesa@yahoo.com>
2020-04-13 13:45:26 +0300

    add all changes to html and javascript files

---
+++ js/query.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+    $('#icon3').click(function(){
+        $('#icon3').toggle();
+        $('#para3').toggle();
+    });
+    $("#icons").click(function(){
+        $("#icons").toggle();
+        $("#paragraph").toggle();
+    });
+    $("#icon2").click(function(){
+        $("#icon2").toggle();
+        $("#para2").toggle();
+    });
+    $('#para3').click(function(){
+        $('#icon3').toggle();
+        $('#para3').toggle();
+    });
+    $("#paragraph").click(function(){
+        $("#icons").toggle();
+        $("#paragraph").toggle();
+    });
+    $("#para2").click(function(){
+        $("#icon2").toggle();
+        $("#para2").toggle();
+    });
+});
+
+
+$(document).ready(function(){
+
+})
